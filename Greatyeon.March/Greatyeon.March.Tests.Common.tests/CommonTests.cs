446780ea389b7ede0c62df503bcfbfe2ac69a2bd
greatyeon <greatyeon@outlook.com>
2017-03-11 16:21:51 +0900

    List count test

---
+++ Greatyeon.March/Greatyeon.March.Tests.Common.tests/CommonTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greatyeon.March.Tests.Common.tests
+{
+    [TestClass]
+    public class CommonTests
+    {
+        [TestMethod]
+        public void ListTest()
+        {
+            IList<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(3);
+            numbers.Add(5);
+            numbers.Add(7);
+            numbers.Add(9);
+
+            Assert.IsTrue(numbers.Count() > 0);
+
+        }
+    }
+}
